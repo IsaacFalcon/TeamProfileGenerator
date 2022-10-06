7035a92e0d71ede5c9c0778e301fe2fd2c342688
Isaac Falcon <ice6490@gmail.com>
2022-10-06 17:09:28 -0500

    added new js files, functions, constructors, and supers for Employee, Intern, Manager, and Engineer. Also added tests in jest for employee constructors and methods. added packages.

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
+const teamArray = [];
+
+function createManager() {
+    console.log("Please build your team");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the team manager's name?",
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the team manager's id?",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the team manager's email?",
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the team manager's office number?",
+      }
+    ]).then(answers => {
+      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+      teamArray.push(manager);
+      //addMember function to re-run prompt
+    });
+  }
